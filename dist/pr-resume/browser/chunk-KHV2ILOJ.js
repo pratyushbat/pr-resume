a095032463f46971b43dad4c107c45b298251f1a
Pratyush Kumar <pratyush3030gmail.compratyush3030gmail.com>
2024-07-19 14:14:51 +0530

    added cahnges

---
+++ dist/pr-resume/browser/chunk-KHV2ILOJ.js
@@ -0,0 +1,1 @@
+import{d as e}from"./chunk-6MDQTQU3.js";var p=e((s,t)=>{t.exports=XMLHttpRequest});export default p();
---
+++ dist/pr-resume/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GHZ5JCVP.mjs";import"./chunk-I6I6TEAN.mjs";import"./chunk-STQQJZO3.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
